f19313733e098f9ce3df25b685ce54518fb7cee5
Ch3rr13 <60113072+Ch3rr13@users.noreply.github.com>
2022-03-10 12:25:42 -0500

    Update Edge.h

--- COEN244/COEN244/Edge.h
+++ COEN244/COEN244/Edge.h
@@ -1,6 +1,25 @@
 #pragma once
+#include "Vertex.h"
+
 class Edge
 {
-	//Edge class
+private:
+	int weight;
+	Vertex* startVertex;
+	Vertex* endVertex;
+
+public:
+	Edge();//Default constructor
+
+	void setWeight(int);
+	int getWeight();
+
+	void setStartVertex(Vertex&);
+	Vertex getStartVertex();
+
+	void setEndVertex(Vertex&);
+	Vertex getEndVertex();
+
+	~Edge();//Destructor
 };
 
