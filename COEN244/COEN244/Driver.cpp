b97ad0bf0c382c0c9f394e9c164494003e445a13
Ch3rr13 <60113072+Ch3rr13@users.noreply.github.com>
2022-03-10 21:49:31 -0500

    Created constructors for Driver class

--- COEN244/COEN244/Driver.cpp
+++ COEN244/COEN244/Driver.cpp
@@ -1,1 +1,21 @@
 #include "Driver.h"
+
+Driver::Driver() {
+	vArray = new Vertex[20];//Predetermined maximums
+	eArray = new Edge[20];
+}
+
+Driver::Driver(Driver& d) {
+	vArray = d.vArray;
+	eArray = d.eArray;
+}
+
+Driver::Driver(Vertex* vArray, Edge* eArray) {
+	this->vArray = vArray;
+	this->eArray = eArray;
+}
+
+Driver::~Driver() {
+	delete[] vArray;
+	delete[] eArray;
+}
--- COEN244/COEN244/Graph.cpp
+++ COEN244/COEN244/Graph.cpp
@@ -1,17 +1,21 @@
 #include "Graph.h"
 
 Graph::Graph() {
-
+	vArray = new Vertex[20];//Predetermined maximums
+	eArray = new Edge[20];
 }
 
 Graph::Graph(Graph& g) {
-
+	vArray = g.vArray;
+	eArray = g.eArray;
 }
 
-Graph::Graph(Vertex*, Edge*) {
-
+Graph::Graph(Vertex* vArray, Edge* eArray) {
+	this->vArray = vArray;
+	this->eArray = eArray;
 }
 
 Graph::~Graph() {
-
+	delete[] vArray;
+	delete[] eArray;
 }
