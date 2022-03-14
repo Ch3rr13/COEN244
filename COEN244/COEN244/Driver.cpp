87270e9c9f60735891a8b27eda60a22d6ab86f53
mycoal2 <michaelhong136@gmail.com>
2022-03-13 23:33:13 -0400

    addVertices and addEdges
    Added a parameter for the size of the array of vertices/edges added

--- COEN244/COEN244/Driver.cpp
+++ COEN244/COEN244/Driver.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Driver.h"
 
 Driver::Driver() {
@@ -11,12 +12,23 @@ Driver::Driver(Driver& d) {
 }
 
 Driver::Driver(Vertex* vArray, Edge* eArray) {
+	this->vArray = new Vertex[20];
+	this->eArray = new Edge[20];
 	this->vArray = vArray;
 	this->eArray = eArray;
 }
 
-bool Driver::addVertices(Vertex* vArray) {
-
+bool Driver::addVertices(Vertex* vArray, int size) {
+	for (int i = 0; i < 20; i++) {
+		if (this->vArray[i].getID() == 0) {//check if the vertice is empty
+			for (int j = 0; j < size; j++) { // i dont think this works because 
+				this->vArray[i] = vArray[j];
+				i++;
+			}
+			return true;
+		}
+	}
+	return false;
 }
 
 Vertex Driver::searchEndVertex(Vertex& vertex) {
@@ -31,8 +43,18 @@ bool Driver::searchValueExists(std::string value) {
 
 }
 
-bool Driver::addEdges(Edge* eArray) {
-
+bool Driver::addEdges(Edge* eArray, int size) {
+	//same as addVertices
+	for (int i = 0; i < 20; i++) {
+		if (this->eArray[i].getWeight() == 0) {//check if the vertice is empty
+			for (int j = 0; j < size; j++) { // i dont think this works because 
+				this->eArray[i] = eArray[j];
+				i++;
+			}
+			return true;
+		}
+	}
+	return false;
 }
 
 bool Driver::searchEdgeExists(Edge& edge) {
